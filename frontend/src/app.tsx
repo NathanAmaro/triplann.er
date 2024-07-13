d860a4888b149455d61f2d209458e716223e64d7
Nathan Amaro <nathannael_g3@hotmail.com>
2024-07-13 13:45:16 -0300

    Inclusão de elementos na tela inicial

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,3 +1,7 @@
+import { ArrowRight, Calendar, MapPin } from "lucide-react"
+
+// AULA 01 - 43:26
+
 export function App() {
 
   return (
@@ -7,12 +11,26 @@ export function App() {
           Convide seus amigos e planeje sua próxima viagem!
         </p>
 
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center justify-center shadow-shape">
-          <input type="text" placeholder="Para onde você vai?" />
-          <input type="text" placeholder="Quando?" />
-          <button>Continuar</button>
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center justify-center shadow-shape gap-3">
+
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400" />
+            <input className="bg-transparent text-lg placeholder-zinc-400 w-full outline-none" type="text" placeholder="Para onde você vai?" />
+          </div>
+
+          <div className="flex items-center gap-2 w-48">
+            <Calendar className="size-5 text-zinc-400" />
+            <input className="bg-transparent text-lg placeholder-zinc-400 w-auto outline-none" type="text" placeholder="Quando?" />
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800"/>
+
+          <button className="bg-lime-300 text-lime-950 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
+            Continuar
+            <ArrowRight className="size-5 text-lime-950"/>
+          </button>
         </div>
-        
+
         <p className="text-zinc-500 text-sm">
           Ao planejar sua viagem pela Triplann.er você automaticamente concorda <br />
           com nossos <a href="" className="text-zinc-300 underline">termos de uso</a> e <a href="" className="text-zinc-300 underline">políticas de privacidade</a>.
